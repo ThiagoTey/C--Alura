77b5d98dbbeda0aef7a1dfc8437b76aa672f61ed
Thiago <teyextreme@outlook.com>
2024-11-28 17:37:14 -0300

    trocar Usuário commit

---
+++ Adicinha/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int continuar = 1;
+
+Console.WriteLine("Jogo de adivinhação de número!");
+
+while (continuar == 1)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(1, 100);
+    Console.WriteLine("Jogo de adivinhação de número!");
+    Console.Write("Digite um número de 1 a 100: ");
+
+    int numeroSelecionado = 0;
+
+    while (numeroSelecionado != randomNumber)
+    {
+        string numeroSelecionadoString = Console.ReadLine()!;
+        numeroSelecionado = int.Parse(numeroSelecionadoString);
+
+        if (randomNumber > numeroSelecionado)
+        {
+            Console.WriteLine($"Número é maior que {numeroSelecionado}");
+            Console.Write("Digite novamente : ");
+        }
+        else if (randomNumber < numeroSelecionado)
+        {
+            Console.WriteLine($"Número é menor que {numeroSelecionado}");
+            Console.Write("Digite novamente : ");
+        }
+
+    }
+
+    Console.WriteLine($"Parabéns! o número era {randomNumber}");
+
+
+    Console.WriteLine("Deseja Continuar?");
+    Console.WriteLine("Digite Y para sim - Outro para não");
+    string escolha = Console.ReadLine()!;
+
+    if (escolha.ToUpper() == "Y")
+    {
+        Console.WriteLine("Continuar");
+    } else
+    {
+        Console.WriteLine("Finalizar");
+        continuar = 2;
+    }
+}
