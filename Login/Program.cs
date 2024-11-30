19116d51e835ebb0d2bb3727213e0346827f6eed
Thiago <teyextreme@outlook.com>
2024-11-30 11:27:14 -0300

    Atividade Login

---
+++ Login/Program.cs
@@ -0,0 +1,29 @@
+﻿Dictionary<string, string> usuarios =  new Dictionary<string, string>
+{
+    {"teste", "senha" },
+    {"usuario", "senha2" }
+};
+
+void login()
+{
+    Console.WriteLine("Insira seu usuario : ");
+    string nomeUsuario = Console.ReadLine()!;
+    Console.WriteLine("Insira sua senha: ");
+    string senhaUsuario = Console.ReadLine()!;
+
+    if(usuarios.ContainsKey(nomeUsuario))
+    {
+        if (usuarios[nomeUsuario] == senhaUsuario)
+        {
+            Console.WriteLine("Logado com sucesso!");
+        } else
+        {
+            Console.WriteLine("Senha incorreta");
+        }
+    } else
+    {
+        Console.WriteLine("Usuário incorreto");
+    }
+}
+
+login();
